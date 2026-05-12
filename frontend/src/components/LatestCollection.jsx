72036fc937ce01f122e869f257335f87968706b1
pa526 <p7266634@gmail.com>
2026-05-12 12:50:58 +0530

    fix some bugs

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react"
 import { ShopContext } from "../context/ShopContext"
-import Title from "./title";
+import Title from "./Title";
 import ProductItem from "./ProductItem";
 
 export const LatestCollection = () => {
